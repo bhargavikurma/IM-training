d06941a6c58044d31feefe21678ea8300cba9c0b
bhargavikurma <107392139+bhargavikurma@users.noreply.github.com>
2022-06-13 14:25:18 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,17 @@
+unsigned int reverseBits(unsigned int n)
+{
+    unsigned int rev = 0;
+    while (n > 0) {
+        rev <<= 1;
+        if (n & 1 == 1)
+            rev ^= 1;
+        n >>= 1;
+    }
+    return rev;
+}
+int main()
+{
+    unsigned int n = 11;
+    printf("%u",reverseBits(n));
+    return 0;
+}
