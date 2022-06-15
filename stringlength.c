5bd23bb8c8e0d383f223413cba2466bb153eee9b
bhargavikurma <107392139+bhargavikurma@users.noreply.github.com>
2022-06-15 12:08:12 +0530

    Update stringlength.c

--- stringlength.c
+++ stringlength.c
@@ -1,16 +1,17 @@
-include <stdio.h>
-#include <string.h>
+#include <stdio.h>
 int main()
 {
-  char a[100];
-  int length;
+  char s[1000];
+  int c = 0;
 
-  printf("Enter a string to calculate its length\n");
-  gets(a);
+  printf("Input a string\n");
+  gets(s);
 
-  length = strlen(a);
+  while (s[c] != '\0')
+    c++;
 
-  printf("Length of the string = %d\n", length);
+  printf("Length of the string: %d\n", c);
 
   return 0;
 }
+
