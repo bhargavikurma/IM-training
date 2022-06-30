9bf3d89ceef9aa7c934c390b7891f4982d8394d9
bhargavikurma <107392139+bhargavikurma@users.noreply.github.com>
2022-06-30 16:10:54 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,7 +1,9 @@
+#include<stdio.h>
 unsigned int reverseBits(unsigned int n)
 {
     unsigned int rev = 0;
-    while (n > 0) {
+    while (n > 0) 
+    {
         rev <<= 1;
         if (n & 1 == 1)
             rev ^= 1;
